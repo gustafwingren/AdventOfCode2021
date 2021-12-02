3628b27f5c0af4f49318b67985308ca707ea2d6b
Gustaf Wingren <gustaf.wingren@sigma.se>
2021-12-02 09:01:52 +0100

    Day2

--- AdventOfCode2021/AdventOfCode2021/Program.cs
+++ AdventOfCode2021/AdventOfCode2021/Program.cs
@@ -10,10 +10,66 @@ namespace AdventOfCode2021
         static void Main(string[] args)
         {
             Day1Part1();
+            Day1Part2();
+            Day2Part1();
             Day2Part2();
         }
 
         private static void Day2Part2()
+        {
+            var input = GetDay2PuzzleInput();
+
+            var horizontalPos = 0;
+            var verticalPos = 0;
+            var aim = 0;
+
+            foreach (var (item1, item2) in input)
+            {
+                switch (item1)
+                {
+                    case 0:
+                        aim -= item2;
+                        break;
+                    case 1:
+                        aim += item2;
+                        break;
+                    case 2:
+                        horizontalPos += item2;
+                        verticalPos += aim * item2;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Day2 Part2: {horizontalPos * verticalPos}");
+        }
+
+        private static void Day2Part1()
+        {
+            var input = GetDay2PuzzleInput();
+
+            var horizontalPos = 0;
+            var verticalPos = 0;
+
+            foreach (var (item1, item2) in input)
+            {
+                switch (item1)
+                {
+                    case 0:
+                        horizontalPos -= item2;
+                        break;
+                    case 1:
+                        horizontalPos += item2;
+                        break;
+                    case 2:
+                        verticalPos += item2;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Day2 Part1: {horizontalPos * verticalPos}");
+        }
+
+        private static void Day1Part2()
         {
             var entries = GetDay1PuzzleInput();
 
@@ -52,6 +108,22 @@ namespace AdventOfCode2021
         private static IEnumerable<int> GetDay1PuzzleInput()
         {
             return File.ReadLines("../../../Day1Input.txt").Select(x => Convert.ToInt32(x));
+        }
+
+        private static IEnumerable<Tuple<int, int>> GetDay2PuzzleInput()
+        {
+            return File.ReadLines("../../../Day2Input.txt").Select(x =>
+            {
+                var items = x.Trim().Split(' ');
+
+                return items[0] switch
+                {
+                    "up" => Tuple.Create(0, Convert.ToInt32(items[1])),
+                    "down" => Tuple.Create(1, Convert.ToInt32(items[1])),
+                    "forward" => Tuple.Create(2, Convert.ToInt32(items[1])),
+                    _ => Tuple.Create(0, 0)
+                };
+            });
         }
     }
 }
